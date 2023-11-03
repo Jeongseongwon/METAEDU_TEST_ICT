f9743513e9eb966968cfdff319eda0efc78de3e6
Jeongseongwon <j001656100@gmail.com>
2023-11-03 19:08:03 +0900

    1103
    *그래프 가시화 구현완료

--- Assets/Chart And Graph/Script/Utils/Multiple Graph Demo/MultipleGraphDemo.cs
+++ Assets/Chart And Graph/Script/Utils/Multiple Graph Demo/MultipleGraphDemo.cs
@@ -17,18 +17,43 @@ public class MultipleGraphDemo : MonoBehaviour
 
         List<DoubleVector2> animationPoints = new List<DoubleVector2>();
         float x = 0f;
+        float y = 0f;
         Graph.HorizontalValueToStringMap.Add(10, "Ten");
-        Graph.VerticalValueToStringMap.Add(10, "$$");
+        Graph.VerticalValueToStringMap.Add(100, "$$");
         Graph.DataSource.StartBatch(); // calling StartBatch allows changing the graph data without redrawing the graph for every change
-        Graph.DataSource.ClearCategory("Player 2"); // clear the "Player 2" category. this category is defined using the GraphChart inspector
+        Graph.DataSource.ClearCategory("SW data"); // clear the "Player 2" category. this category is defined using the GraphChart inspector
 
-        for (int i = 0; i < TotalPoints; i++)  //add random points to the graph
-        {
-            Graph.DataSource.AddPointToCategory("Player 1", System.DateTime.Now + System.TimeSpan.FromDays(x), Random.value * 20f + 10f); // each time we call AddPointToCategory 
-            Graph.DataSource.AddPointToCategory("Player 2", System.DateTime.Now + System.TimeSpan.FromDays(x), Random.value * 20f + 10f); // each time we call AddPointToCategory 
-            //animationPoints.Add(new DoubleVector2(ChartDateUtility.DateToValue(System.DateTime.Now + System.TimeSpan.FromDays(x)), Random.value * 10f));
-            x += 3;
-        }
+
+        Graph.DataSource.AddPointToCategory("OX data", 0, 0); // each time we call AddPointToCategory 
+        Graph.DataSource.AddPointToCategory("SW data", 0, 0); // each time we call AddPointToCategory 
+
+        Graph.DataSource.AddPointToCategory("OX data", 5, 7); // each time we call AddPointToCategory 
+        Graph.DataSource.AddPointToCategory("SW data", 5, 3); // each time we call AddPointToCategory 
+
+        Graph.DataSource.AddPointToCategory("OX data", 10, 2); // each time we call AddPointToCategory 
+        Graph.DataSource.AddPointToCategory("SW data", 10, 9); // each time we call AddPointToCategory 
+
+
+        //for (int i = 0; i < TotalPoints; i++)  //add random points to the graph
+        //{
+        //    Graph.DataSource.AddPointToCategory("OX data", 0, 0); // each time we call AddPointToCategory 
+        //    Graph.DataSource.AddPointToCategory("SW data", 0, 0); // each time we call AddPointToCategory 
+
+        //    Graph.DataSource.AddPointToCategory("OX data", 5, 7); // each time we call AddPointToCategory 
+        //    Graph.DataSource.AddPointToCategory("SW data", 5, 3); // each time we call AddPointToCategory 
+
+        //    Graph.DataSource.AddPointToCategory("OX data", 10, 2); // each time we call AddPointToCategory 
+        //    Graph.DataSource.AddPointToCategory("SW data", 10, 9); // each time we call AddPointToCategory 
+
+
+
+        //    //Graph.DataSource.AddPointToCategory("OX data", x, y+10); // each time we call AddPointToCategory 
+        //    //Graph.DataSource.AddPointToCategory("SW data", x, y); // each time we call AddPointToCategory 
+        //    //Graph.DataSource.AddPointToCategory("Player 2", System.DateTime.Now + System.TimeSpan.FromDays(x), Random.value * 20f + 10f); // each time we call AddPointToCategory 
+        //    //animationPoints.Add(new DoubleVector2(ChartDateUtility.DateToValue(System.DateTime.Now + System.TimeSpan.FromDays(x)), Random.value * 10f));
+        //    x += 1; 
+        //    y += 50;
+        //}
 
         Graph.DataSource.EndBatch(); // finally we call EndBatch , this will cause the GraphChart to redraw itself
         //if (Animation != null)
